8d61fcc1f1a1831663bef9ee9adf2475d6719c44
vutuankien <vutuankien030904@gmail.com>
2024-11-16 16:19:03 +0700

    day 3: basic routing and form default behavior

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
+const { log } = require('console')
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
 const port = 3000
 const path = require('path')
-console.log("Path: ",__dirname);
-console.log("Image Path: ",path.join(__dirname,'public'));
+// console.log("Path: ",__dirname);
+// console.log("Image Path: ",path.join(__dirname,'public'));
 app.use(express.static(path.join(__dirname,'public')))
 
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json()); 
 
 // HandleBars requirement
 const handlebars = require('express-handlebars').engine;
@@ -18,10 +21,10 @@ app.engine('hbs', handlebars({
 }))
 app.set('view engine', 'hbs')
 app.set('views',path.join(__dirname,'resources/views'))
-console.log('View Path:',path.join(__dirname,'resources/views'))
+// console.log('View Path:',path.join(__dirname,'resources/views'))
 
 //http logger
-app.use(morgan('combined'))
+// app.use(morgan('combined'))
 
 app.get('/', (req, res) => {
     return res.render('home')
@@ -33,4 +36,30 @@ app.get('/about', (req, res) => {
     return res.render('about')
 })
 
+app.get("/search", (req, res) => {
+    console.log(req.query.q);
+    return res.render('search')
+})
+
+app.post("/search", (req, res) => {
+    console.log(req.body);
+    return res.send('search')
+})
+
 app.listen(port, () => console.log(`Server is running on http://localhost:${port}`))
+
+
+// Req = request
+//res = response
+
+/**
+ * Phương thức GET
+ * Mặc định trình duyệt là GET
+ * 
+ */
+
+//TODO:Query Parameter
+/**
+ * address/search?q= name1 % q = name2
+ * 
+ */
